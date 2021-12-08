2a81bb96d7cf17c47ec02738f4b951982ef8bf01
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2021-12-08 16:10:00 -0500

    Able to get categories from AJAX request

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,17 +1,33 @@
-// import { useState } from 'react'
-// import { useEffect } from 'react';
-// import axios from 'axios';
+import { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export const App = () => {
+  // const categories = ["vehicles", "Entertainment: Music", "Geography"]
+  const [categories, setCategories] = useState([]) 
+  
+  useEffect(() => {
+    console.log('USE EFFECT IS RUNNING')
+    //AJAX request:
+    axios
+        .get('https://opentdb.com/api.php?amount=10')
+        .then((response) => {
+          const results = response.data.results
+          console.log(results)
+          for (let result of results) {
+            console.log(result.category)
+          }
+          setCategories(results.map((resultsObj) => resultsObj.category))
+        })
+  }, [])
+  console.log(categories)
   return (
-    <h1>Hola mundo!</h1>
+    <>
+      <h1>Category List</h1>
+      {categories}
+    </>
   )
 }
-// useEffect(() => {
-//     console.log('USE EFFECT IS RUNNING')
-//     //AJAX request:
-//     axios
-//         .get( 'https://opentdb.com/api.php?amount=10')
-// })
+
 
 export { App as default }
